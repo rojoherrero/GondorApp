7e9e302718c350536f9f64dc05a8f2a945968d4a
rojoherrero <rojoherrero@gmail.com>
2016-09-18 17:50:57 +0200

    shorten class name

--- pharmacy-project/src/main/java/org/rojoherrero/gondor/pharmacy/portlet/constants/PharmacyPortletKeys.java
+++ pharmacy-project/src/main/java/org/rojoherrero/gondor/pharmacy/portlet/constants/PharmacyPortletKeys.java
@@ -1,5 +1,7 @@
 package org.rojoherrero.gondor.pharmacy.portlet.constants;
 
-public class PharmacyPortletWebKeys {
+public class PharmacyPortletKeys {
 	public static final String PHARMACY_PORTLET = "Pharmacy_Portlet";
+	public static final String POST_DELETE_DRUG = "/pharmacy/drug/delete";
+	public static final String POST_NEW_DRUG = "/pharmacy/add/new_drug";
 }
